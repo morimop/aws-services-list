8986bcab4c0cfaa1dc96cb21781673a661b1205b
morimop <morimop@gmail.com>
2017-11-04 16:55:30 +0900

    change using module from chromy to cheerio-httpcli

--- index.js
+++ index.js
@@ -1,70 +1,73 @@
-const Chromy = require('chromy');
+let client = require('cheerio-httpcli');
 const fs = require("fs");
 
 let targetUri = 'https://aws.amazon.com/';
-let chromy = new Chromy();
 let lang = '';
 
 async function scrap() {
-  await chromy.blockUrls(['*.ttf', '*.gif', '*.png', '*.jpg', '*.jpeg', '*.webp']);
   if (process.argv[2]) {
     lang = process.argv[2];
     console.log('switch-language:'+ lang);
-    targetUri = targetUri + lang + '/';
+    if(lang != 'en') {
+      targetUri = targetUri + lang + '/';
+    }
+    client.set('headers', {
+      'Accept-Language': lang
+    });
   }
   console.log("goto:" + targetUri);
-  await chromy.goto(targetUri);
-  await chromy.wait(3000);
-  const services = await chromy.evaluate(() => {
-    const contents = document.querySelectorAll('div.aws-nav-mm-section');
+  client.fetch(targetUri,(err, $, res, body)=>{
+    const contents = $('div.aws-nav-mm-section');
     let services = [];
     [].map.call(contents, (d)=>{
-      const headerTitle = d.querySelector('div.aws-nav-mm-section-header').textContent.trim();
-      const serviceList = [].map.call(d.querySelectorAll('li'),(l)=>{
-          return {name: l.textContent,href: l.querySelector('a').href};
+      const headerTitle = $(d).children('div.aws-nav-mm-section-header').text().trim();
+      const serviceList = [].map.call($(d).find('li'),(l)=>{
+        let srvHref = $(l).children('a').attr('href');
+        if (srvHref.startsWith('/')) {
+          srvHref = targetUri + srvHref.slice(1);
+        }
+        return {
+          name: $(l).text(),
+          href: srvHref
+        };
       });
       services.push({
           category: headerTitle,
           services: serviceList
       });
     });
-    return services;  
-  });
-  for (let sg of services) {
-    for (let s of sg.services){
-      console.log(s.href);
-      await chromy.goto(s.href);
-      await chromy.wait(500);
-      const abstructEN = await chromy.evaluate(() => {
-          if(document.querySelector('main')){
-              const pElements = [].filter.call(
-                document.querySelector('main').querySelectorAll('p'),
-                (pn)=>{
-                  if(!pn.querySelector('a')){
-                    return true;
-                  };
-                  return (pn.querySelector('a').textContent != pn.textContent);
-                });
-              let pText = pElements[0].textContent.trim();
-              if (pElements.length > 1) {
-                  pText = pText + "\r\n" + pElements[1].textContent.trim();
-              }
-              return pText;
+    //console.log(JSON.stringify(services,null,2));
+    for (let sg of services) {
+      for (let s of sg.services){
+        console.log(s.href);
+        let res = client.fetchSync(s.href);
+        const abstruct = (($) => {
+          if($('main').length == 1){
+            const pElements = [].filter.call(
+              $('main').find('p'),
+              (pn)=>{
+                if($(pn).find('a').length == 0){
+                  return true;
+                };
+                return ($(pn).find('a').text() != $(pn).text());
+              });
+            let pText = $(pElements[0]).text().trim();
+            if (pElements.length > 1) {
+                pText = pText + "\r\n" + $(pElements[1]).text().trim();
+            }
+            return pText;
           }
-          return document.querySelector('p').parentNode.textContent.trim();
-      });
-      s.abstructEN = abstructEN;
-      console.log(abstructEN);
+          return $('p').parent().text().trim();
+        })(res.$);
+        s.abstruct = abstruct;
+        console.log(abstruct);
+      };
     };
-  };
-
-  await chromy.close();
-  //console.log(JSON.stringify(services));
-  fs.writeFileSync("./tmp/services"+lang+".json", JSON.stringify(services));
+    fs.writeFileSync("./tmp/services"+lang+".json", JSON.stringify(services));
+  });
 }
 
 scrap().catch(err => {
     console.error("Error.");
     console.log(err);
-    chromy.close();
 });
